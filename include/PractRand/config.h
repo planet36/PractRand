9229884b5c01616284a28983b77931a421539ed4
Steven Ward <planet36@users.noreply.github.com>
2023-07-13 14:40:46 -0500

    Use static_assert

--- include/PractRand/config.h
+++ include/PractRand/config.h
@@ -71,14 +71,14 @@ namespace PractRand {
 	typedef signed long long int Sint64;
 
 	//double-checking those integer sizes:
-	typedef char _compiletime_assert_uint8_size [(sizeof(Uint8 )==1) ? 1 : -1];
-	typedef char _compiletime_assert_uint16_size[(sizeof(Uint16)==2) ? 1 : -1];
-	typedef char _compiletime_assert_uint32_size[(sizeof(Uint32)==4) ? 1 : -1];
-	typedef char _compiletime_assert_uint64_size[(sizeof(Uint64)==8) ? 1 : -1];
-	typedef char _compiletime_assert_sint8_size [(sizeof(Sint8 )==1) ? 1 : -1];
-	typedef char _compiletime_assert_sint16_size[(sizeof(Sint16)==2) ? 1 : -1];
-	typedef char _compiletime_assert_sint32_size[(sizeof(Sint32)==4) ? 1 : -1];
-	typedef char _compiletime_assert_sint64_size[(sizeof(Sint64)==8) ? 1 : -1];
+	static_assert(sizeof(Uint8 )==1);
+	static_assert(sizeof(Uint16)==2);
+	static_assert(sizeof(Uint32)==4);
+	static_assert(sizeof(Uint64)==8);
+	static_assert(sizeof(Sint8 )==1);
+	static_assert(sizeof(Sint16)==2);
+	static_assert(sizeof(Sint32)==4);
+	static_assert(sizeof(Sint64)==8);
 }
 
 /*
